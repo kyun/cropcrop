7b94b23f4a9a0ff288a2b3a2085ed8926a6591e8
Kyun <kyun@hpcnt.com>
2022-03-26 13:32:18 +0900

    123

---
+++ src/components/MyCropper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PADDING = 128;
+const IMG = './Lenna.png';
+
+const MyCropper: React.FC<any> = () => {
+  //
+
+  return <div>123</div>;
+};
+
+export default MyCropper;
